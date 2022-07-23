f6742c32ea28b7a911d2ad9ba9e22394430077ca
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-07-23 19:30:34 +0530

    commit 1.1

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -11,25 +11,48 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView coffeeNos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coffeeNos = findViewById(R.id.Quantity);
+        Toast.makeText(this,"Ordered ..",Toast.LENGTH_SHORT).show();
     }
 
     public void order(View v){
-        display(1);
-        displayPrice(2*5);
-        Toast.makeText(this,"ordered ..",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Order successful", Toast.LENGTH_SHORT).show();
     }
 
     private void display(int i){
-        TextView quantityOfOrder = findViewById(R.id.Quantity);
-        quantityOfOrder.setText(i);
+        coffeeNos.setText(String.valueOf(i));
     }
 
     private void displayPrice(int i){
         TextView priceOf = findViewById(R.id.price);
         priceOf.setText(NumberFormat.getCurrencyInstance().format(i));
+
+
+    }
+
+    public void increase(View v){
+        int val = Integer.parseInt(coffeeNos.getText().toString());
+        val++;
+        display(val);
+        displayPrice(val*5);
+    }
+
+    public void decrease(View v){
+
+        int val = Integer.parseInt(coffeeNos.getText().toString());
+
+        if(val>0) {
+            val--;
+            display(val);
+            displayPrice(val * 5);
+        }else{
+            Toast.makeText(MainActivity.this, "Minimum no of order reached..", Toast.LENGTH_SHORT).show();
+        }
     }
 }
