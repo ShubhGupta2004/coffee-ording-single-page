e4a4c9142d7334a7780ac4b0e7833222354c9031
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-07-24 22:41:03 +0530

    commit 1.7

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.text.NumberFormat;
 
@@ -13,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     public TextView coffeeNos;
     public int price = 9;
+    public EditText name;
+    public int finalPrice=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +29,24 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Ordered ..",Toast.LENGTH_SHORT).show();
     }
 
+    @SuppressLint("SetTextI18n")
     public void order(View v){
         TextView finalMsg = findViewById(R.id.finalMessage);
         String msg = "Your order of "+coffeeNos.getText()+" coffee had been successfully ordered";
+        name = findViewById(R.id.getName);
+        String Name = name.getText().toString();
+
+
+        TextView SummaryName = findViewById(R.id.orderSummaryName);
+        SummaryName.setText("Name: "+Name);
+
+        TextView SummaryQuantity = findViewById(R.id.orderSummaryQuantity);
+        SummaryQuantity.setText("Quantity: "+coffeeNos.getText().toString());
+
+        TextView summaryPrice = findViewById(R.id.orderSummaryFinalPrice);
+        summaryPrice.setText("Final Price: "+String.valueOf(finalPrice));
+
+
         finalMsg.setText(msg);
     }
 
@@ -42,8 +64,9 @@ public class MainActivity extends AppCompatActivity {
     public void increase(View v){
         int val = Integer.parseInt(coffeeNos.getText().toString());
         val++;
+        finalPrice=val*price;
         display(val);
-        displayPrice(val*price);
+        displayPrice(finalPrice);
     }
 
     public void decrease(View v){
@@ -52,10 +75,23 @@ public class MainActivity extends AppCompatActivity {
 
         if(val>0) {
             val--;
+            finalPrice=val*price;
             display(val);
-            displayPrice(val * price);
+            displayPrice(finalPrice);
         }else{
             Toast.makeText(MainActivity.this, "Minimum no of order reached..", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void whipped(View v){
+        CheckBox checkBox = findViewById(R.id.whip);
+        int no = Integer.parseInt(coffeeNos.getText().toString());
+        if(checkBox.isChecked()){
+            finalPrice=finalPrice+no*5;
+            displayPrice(finalPrice);
+        }else{
+            finalPrice=finalPrice-no*5;
+            displayPrice(finalPrice);
         }
     }
 }
